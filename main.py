e6f9c5be6290f9819e6ba5f89ef8818cc6bb982f
Thiago Gerbi <gerbithiago@gmail.com>
2025-03-26 01:42:54 -0300

    Criando funções para o cadastro de conta corrente, verificando se o usuário cadastrado jaá possui conta corrente e função de login funcionando

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ menu = """
 
 
 clientes = {}
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
 LIMITE_SAQUES = 3
 
 def cadastrar_cliente(clientes):
@@ -75,11 +71,104 @@ def cadastrar_cliente(clientes):
             "bairro_cliente": bairro_cliente,
             "cidade_cliente": cidade_cliente,
             "estado_cliente": estado_cliente,
+        },
+        "conta_corrente": {
+            "possui_conta_corrente": False,
+            "numero_conta_corrente": None,
+            "agencia": ""
         }
     }
 
     print(f"\n Cliente {nome_cliente} cadastrado com sucesso!")
 
+    return clientes
+
+def login(clientes):
+
+    while True:
+        verifica_cpf = int(input("Insira o seu CPF (Apenas números): "))
+        if len(str(verifica_cpf)) == 11:
+                if verifica_cpf in clientes:
+                    nome_cliente = clientes[verifica_cpf]["nome_cliente"]
+                    print(f"Seja bem vindo {nome_cliente}")
+                    
+                    verifica_conta_corrente(clientes= clientes, cpf_cliente= verifica_cpf)
+                    exibe_menu_cliente(clientes= clientes)
+                    return verifica_cpf
+                else:
+                    print("CPF não encontrado! Tente novamente ou cadastre-se")
+                    break
+        else:
+                print("CPF inválido! Digite apenas números com 11 dígitos.")
+
+def gerar_numero_conta(clientes):
+    """Gera o próximo número de conta baseado no maior número existente."""
+    numeros_contas = [
+        dados["conta_corrente"]["numero_conta_corrente"]
+        for dados in clientes.values()
+        if dados["conta_corrente"]["numero_conta_corrente"] is not None
+    ]
+    
+    return max(numeros_contas, default=0) + 1  # Se não houver contas, começa em 1000
+
+
+
+def verifica_conta_corrente(clientes, cpf_cliente):
+
+    status_conta_corrente = clientes[cpf_cliente]["conta_corrente"]["possui_conta_corrente"]
+
+    if not status_conta_corrente:
+        print("Você ainda não criou sua conta corrente! Deseja criar agora? ")
+        escolha = input("""
+
+            [s] Sim
+            [n] Não
+        
+        
+        """)
+
+        if escolha.lower() == "s":
+
+            clientes[cpf_cliente]["conta_corrente"]["possui_conta_corrente"] = True
+            numero_nova_conta = gerar_numero_conta(clientes)
+            clientes[cpf_cliente]["conta_corrente"]["numero_conta_corrente"] = numero_nova_conta
+            clientes[cpf_cliente]["conta_corrente"]["agencia"] = "001"
+
+            print(f"Conta corrente criada com sucesso!\nNúmero da conta: {numero_nova_conta}, Agência: 001")
+
+
+        else:
+            print(menu)
+
+
+
+
+def exibe_menu_cliente(clientes):
+ while True:
+
+            opcao = input(menu)
+
+            if opcao.lower() == "d":
+                print("DEPÓSITO:")
+                valor_deposito = float(input("Valor do depósito: "))
+                saldo, extrato = deposito(clientes)
+            
+            elif opcao.lower() == "s":
+                print("SAQUE:")
+                valor_saque = float(input("Valor do saque: "))
+                numero_saques, saldo, extrato = saque(numero_saques=numero_saques, saldo=saldo, extrato=extrato, valor_saque=valor_saque)
+            
+            elif opcao.lower() == "e":
+                print("EXTRATO:")
+                mostrar_extrato(extrato=extrato, saldo=saldo)
+
+
+            elif opcao.lower() == "q":
+                print("Saindo...")
+                break
+
+            else:
+                print("Operação inválida. Selecione novamente")
 
 def deposito(saldo, extrato, valor_deposito):
 
@@ -98,7 +187,7 @@ def saque(saldo, extrato, valor_saque, numero_saques):
 
     excedeu_saldo = valor_saque > saldo
 
-    excedeu_limite = valor_saque > limite
+    excedeu_limite = valor_saque > LIMITE_SAQUES
 
     excedeu_saques = numero_saques >= LIMITE_SAQUES
 
@@ -136,31 +225,7 @@ while True:
 
     if escolha_inicio == "1":
         print("Login")
-        while True:
-
-            opcao = input(menu)
-
-            if opcao.lower() == "d":
-                print("DEPÓSITO:")
-                valor_deposito = float(input("Valor do depósito: "))
-                saldo, extrato = deposito(saldo=saldo, extrato=extrato, valor_deposito=valor_deposito)
-            
-            elif opcao.lower() == "s":
-                print("SAQUE:")
-                valor_saque = float(input("Valor do saque: "))
-                numero_saques, saldo, extrato = saque(numero_saques=numero_saques, saldo=saldo, extrato=extrato, valor_saque=valor_saque)
-            
-            elif opcao.lower() == "e":
-                print("EXTRATO:")
-                mostrar_extrato(extrato=extrato, saldo=saldo)
-
-
-            elif opcao.lower() == "q":
-                print("Saindo...")
-                break
-
-            else:
-                print("Operação inválida. Selecione novamente")
+        login(clientes= clientes)
 
 
     elif escolha_inicio == "2":
